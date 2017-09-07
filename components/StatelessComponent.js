12b23ce6bac6723c2a83103a173109f67f99bdaa
ega <radiegtya@yahoo.co.id>
2017-09-07 10:47:52 +0700

    2.component

---
+++ components/StatelessComponent.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import {View, Text} from 'react-native'
+
+const StatelessComponent = () => (
+    <View>
+      <Text>Hello RN from Stateless Component</Text>
+    </View>
+)
+
+export default StatelessComponent
